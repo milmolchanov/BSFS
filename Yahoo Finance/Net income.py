eda6cf3c2156ef64d9397c6be511e148919e7815
milmolchanov <milmolchanov@gmail.com>
2024-06-06 18:00:02 +0300

    Update Net income.py

--- Yahoo Finance/Net income.py
+++ Yahoo Finance/Net income.py
@@ -1,9 +1,8 @@
 import yfinance as yf
 
-tickers = ['XOM']
+tickers = [] # here you can write the tickers of the companies you need. Do not forget ''! For example, for ExxonMobil it will be 'XOM'.
 
 for ticker in tickers:
-    GetCompanyInformation = yf.Ticker(ticker)
     print(ticker,end="")
     print(':',end="")
-    print(GetCompanyInformation.info['returnOnEquity'])
+    print(yf.Ticker(ticker).info['returnOnEquity'])
