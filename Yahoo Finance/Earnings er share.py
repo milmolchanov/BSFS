d9ae927996886777bfaff0af7f16f56fc78938d4
milmolchanov <milmolchanov@gmail.com>
2024-06-06 17:57:15 +0300

    Create Earnings er share.py

---
+++ Yahoo Finance/Earnings er share.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+import csv
+
+tickers = [] # here you can write the tickers of the companies you need. Do not forget ''! For example, for ExxonMobil it will be 'XOM'.
+
+for ticker in tickers:
+    EPS = yf.Ticker(ticker).info["trailingEps"]
+    print(ticker,end="")
+    print(":",end="")
+    print(EPS)
